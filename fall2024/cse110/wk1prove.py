d9a0f434eee1d1e9c8dfacabd01dc2413c7ebd6e
Gage <gagedorlando@gmail.com>
2024-09-18 10:48:00 -0600

    first prove assignment done

---
+++ fall2024/cse110/wk1prove.py
@@ -0,0 +1,6 @@
+color = input('Please type your favorite color: ').strip()
+
+if(color.lower()=='blue'):
+    print ("Blue is the best color, you're so right for that")
+else:
+    print('Yeah, I guess '+color+' is alright')
