52c767427b45c9567a36f519e8e4e50a861b4cf7
Gage <gagedorlando@gmail.com>
2024-09-23 10:40:59 -0600

    Made code simpler

--- fall2024/cse110/wk2prepare.py
+++ fall2024/cse110/wk2prepare.py
@@ -1,4 +1,4 @@
-first = input('What is your first name? ').lower().capitalize()
-last = input('What is your last name? ').lower().capitalize()
+first = input('What is your first name? ').capitalize()
+last = input('What is your last name? ').capitalize()
 
 print(f'Your name is {last}, {first} {last}.')
