32cca973a50921102d503f30c656502ddc25501b
Gage <gagedorlando@gmail.com>
2024-09-30 09:57:28 -0600

    Did wk3 prepare for cse

---
+++ fall2024/cse110/wk3prepare.py
@@ -0,0 +1,7 @@
+age = int(input('How old are you? '))
+print(f'On your next birthday you will be {age+1}\n')
+eggs = int(input('How many egg cartons do you have? '))
+print(f'You have {eggs*12} eggs\n')
+cookies = float(input('How many cookies do you have? '))
+people = float(input('How many people are there? '))
+print(f'Each person may have {cookies/people} cookies')
