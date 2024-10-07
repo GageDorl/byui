b2e6c105179f82768d42505201816bb6e37d6c22
Gage <gagedorlando@gmail.com>
2024-10-07 11:29:31 -0600

    Worked on wk4 stuff

--- fall2024/cse110/wk4prepare.py
+++ fall2024/cse110/wk4prepare.py
@@ -1,3 +1,2 @@
-fahrenheit = float(input('What is the temperature in Fahrenheit? '))
-celsius = (fahrenheit-32)/1.8
+celsius = ((float(input('What is the temperature in Fahrenheit? ')))-32)/1.8
 print(f'The temperature in Celsius is {celsius:.1f} degrees.')
---
+++ fall2024/cse110/wk4teach.py
@@ -0,0 +1,47 @@
+import math
+
+
+
+
+
+
+
+
+m = float(input('Welcome to the velocity calculator. Please enter the following:\n\nMass (in kg): '))
+g = float(input('Gravity (in m/s^2, 9.8 for Earth, 24 for Jupiter): '))
+t = float(input('Time (in seconds): '))
+p = float(input('Density of the fluid (in kg/m^3, 1.3 for air, 1000 for water): '))
+A = float(input('Cross sectional area (in m^2): '))
+C = float(input('Drag constant (0.5 for sphere, 1.1 for cylinder): '))
+c = 1/2*(p*A*C)
+ve = math.sqrt(m*9.8/c) * (1 - math.exp((-math.sqrt(m*9.8*c)/m)*t))
+vj = math.sqrt(m*24/c) * (1 - math.exp((-math.sqrt(m*24*c)/m)*t))
+V = ''
+choice = ''
+alt = ''
+Valt = ''
+def gravityChoice():
+    if g == 9.8:
+        V = ve
+        choice = 'Earth'
+        alt = 'Jupiter'
+        Valt = vj
+    else:
+        if g == 24:
+            V = vj
+            choice = 'Jupiter'
+            alt = 'Earth'
+            Valt = ve
+        else:
+            g = float(input('Please choose either 9.8 or 24 for gravity: '))
+            gravityChoice()
+
+print(f'\nThe inner value of c is: {c:.3f}\nThe velocity after {t:.1f} seconds is: {V:.3f} m/s on {choice}, if you had chosen {alt} it would be {Valt:.3f}')
+bm = 6
+bA = 0.054
+bC = .5
+bc = 1/2*(p*bA*bC)
+bV = math.sqrt(bm*g/bc) * (1 - math.exp((-math.sqrt(bm*9.8*bc)/bm)*t))
+print(f'The velocity of a bowling ball at {t:.1f} seconds is {bV} m/s on {choice}')
+
+
