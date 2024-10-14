6fc58168ea298b77ddca84b89ba8375e02d2420b
Gage <gagedorlando@gmail.com>
2024-10-14 09:06:34 -0600

    Did wk5 prepare, and downloaded art wk5 stuff

---
+++ fall2024/cse110/wk5prepare.py
@@ -0,0 +1,14 @@
+first = int(input('What is the first number? '))
+second = int(input('What is the second number? '))
+if(first>second):
+    print('The first number is greater\nThe numbers are not equal\nThe second number is not greater\n')
+else:
+    if(first<second):
+        print('The first number is not greater\nThe numbers are not equal\nThe second number is greater\n')
+    else:
+        print('The first number is not greater\nThe numbers are equal\nThe second number is not greater\n')
+favorite = str(input('What is your favorite animal? ').lower())
+if(favorite=='shark'):
+    print('That\'s my favorite animal too!')
+else:
+    print('That one is not my favorite')
