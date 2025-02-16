0a3bce63ddc19efb192d846b433eaffec8628f75
Gage <gagedorlando@gmail.com>
2025-02-15 21:11:59 -0700

    new art stuff and cse

---
+++ winter2025/cse111/loops.py
@@ -0,0 +1,23 @@
+for i in range(11):
+    if i != 10:
+        print(i, end = ", ")
+    else:
+        print(i)
+
+for i in range(3,11):
+    if i != 10:
+        print(i, end = ", ")
+    else:
+        print(i)
+
+for i in range(20, 36, 5):
+    if i != 35:
+        print(i, end = ", ")
+    else:
+        print(i)
+
+for i in range(40, 29, -2):
+    if i != 30:
+        print(i, end = ", ")
+    else:
+        print(i)
